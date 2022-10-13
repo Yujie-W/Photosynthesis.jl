b9d79c8183b0a3f734c4f6afe75e1491249d7cc8
Yujie Wang <jesiner@gmail.com>
2022-10-12 18:39:42 -0700

    update t_opt function

--- src/temperature/clm.jl
+++ src/temperature/clm.jl
@@ -1,13 +1,14 @@
 """
 
-    update_clm_vcmax_td!(td::ArrheniusPeakTD{FT}, t_opt::FT) where {FT<:AbstractFloat}
+    update_clm_vcmax_td!(photo_set::C3ParaSet{FT}, t_opt::FT) where {FT<:AbstractFloat}
 
 Update the TD for Vcmax, given
-- `td` `ArrheniusPeakTD` type temperature dependency
+- `photo_set` `C3ParaSet` type parameter set
 - `t_opt` 10 day average temperature in `[K]`
 
 """
-function update_clm_vcmax_td!(td::ArrheniusPeakTD{FT}, t_opt::FT) where {FT<:AbstractFloat}
+function update_clm_vcmax_td!(photo_set::C3ParaSet{FT}, t_opt::FT) where {FT<:AbstractFloat}
+    td = photo_set.VcT;
     td.ΔHa_to_RT25 = 72000.0 / GAS_R() / T₂₅();
     td.ΔHd_to_R = 200000.0 / GAS_R();
     td.ΔSv_to_R = (668.39 - 1.07 * (t_opt - T₀())) / GAS_R();
@@ -19,14 +20,15 @@ end
 
 """
 
-    update_clm_vcmax_td!(td::ArrheniusPeakTD{FT}, t_opt::FT) where {FT<:AbstractFloat}
+    update_clm_vcmax_td!(photo_set::C3ParaSet{FT}, t_opt::FT) where {FT<:AbstractFloat}
 
 Update the TD for Jmax, given
-- `td` `ArrheniusPeakTD` type temperature dependency
+- `photo_set` `C3ParaSet` type parameter set
 - `t_opt` 10 day average temperature in `[K]`
 
 """
-function update_clm_jmax_td!(td::ArrheniusPeakTD{FT}, t_opt::FT) where {FT<:AbstractFloat}
+function update_clm_jmax_td!(photo_set::C3ParaSet{FT}, t_opt::FT) where {FT<:AbstractFloat}
+    td = photo_set.JT;
     td.ΔHa_to_RT25 = 50000.0 / GAS_R() / T₂₅();
     td.ΔHd_to_R = 200000.0 / GAS_R();
     td.ΔSv_to_R = (659.70 - 0.75 * (t_opt - T₀())) / GAS_R();
