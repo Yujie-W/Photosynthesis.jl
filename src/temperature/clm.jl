7643054bd97a337e066fe61181865b6b0c926529
Yujie Wang <jesiner@gmail.com>
2022-10-12 18:41:35 -0700

    combine the clm t+opt functions

--- src/temperature/clm.jl
+++ src/temperature/clm.jl
@@ -1,38 +1,24 @@
 """
 
-    update_clm_vcmax_td!(photo_set::C3ParaSet{FT}, t_opt::FT) where {FT<:AbstractFloat}
+    use_clm_td!(photo_set::C3ParaSet{FT}, t_opt::FT) where {FT<:AbstractFloat}
 
 Update the TD for Vcmax, given
 - `photo_set` `C3ParaSet` type parameter set
 - `t_opt` 10 day average temperature in `[K]`
 
 """
-function update_clm_vcmax_td!(photo_set::C3ParaSet{FT}, t_opt::FT) where {FT<:AbstractFloat}
-    td = photo_set.VcT;
-    td.ΔHa_to_RT25 = 72000.0 / GAS_R() / T₂₅();
-    td.ΔHd_to_R = 200000.0 / GAS_R();
-    td.ΔSv_to_R = (668.39 - 1.07 * (t_opt - T₀())) / GAS_R();
-    td.C = 1 + exp( td.ΔSv_to_R - td.ΔHd_to_R/T₂₅(FT) );
+function use_clm_td!(photo_set::C3ParaSet{FT}, t_opt::FT) where {FT<:AbstractFloat}
+    vtd = photo_set.VcT;
+    vtd.ΔHa_to_RT25 = 72000.0 / GAS_R() / T₂₅();
+    vtd.ΔHd_to_R = 200000.0 / GAS_R();
+    vtd.ΔSv_to_R = (668.39 - 1.07 * (t_opt - T₀())) / GAS_R();
+    vtd.C = 1 + exp( vtd.ΔSv_to_R - vtd.ΔHd_to_R/T₂₅(FT) );
 
-    return nothing
-end
-
-
-"""
-
-    update_clm_vcmax_td!(photo_set::C3ParaSet{FT}, t_opt::FT) where {FT<:AbstractFloat}
-
-Update the TD for Jmax, given
-- `photo_set` `C3ParaSet` type parameter set
-- `t_opt` 10 day average temperature in `[K]`
-
-"""
-function update_clm_jmax_td!(photo_set::C3ParaSet{FT}, t_opt::FT) where {FT<:AbstractFloat}
-    td = photo_set.JT;
-    td.ΔHa_to_RT25 = 50000.0 / GAS_R() / T₂₅();
-    td.ΔHd_to_R = 200000.0 / GAS_R();
-    td.ΔSv_to_R = (659.70 - 0.75 * (t_opt - T₀())) / GAS_R();
-    td.C = 1 + exp( td.ΔSv_to_R - td.ΔHd_to_R/T₂₅(FT) );
+    jtd = photo_set.JT;
+    jtd.ΔHa_to_RT25 = 50000.0 / GAS_R() / T₂₅();
+    jtd.ΔHd_to_R = 200000.0 / GAS_R();
+    jtd.ΔSv_to_R = (659.70 - 0.75 * (t_opt - T₀())) / GAS_R();
+    jtd.C = 1 + exp( jtd.ΔSv_to_R - jtd.ΔHd_to_R/T₂₅(FT) );
 
     return nothing
 end
